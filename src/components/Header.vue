64619d76ebc584d3a12bfef0022e4fa903b003d1
tfd-ed <info@tfdevs.com>
2024-10-08 21:35:56 +0700

    Demo

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <router-link to="/">Home</router-link>
+            </li>
+            <li>
+                <router-link to="/about">About</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import Home from '@/pages/Home.vue'
+import About from '@/pages/About.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: Home },
+        { path: '/about', component: About }
+    ]
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
